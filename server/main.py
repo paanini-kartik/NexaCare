0d7d7e854e66af5abb33a6ee777bdfbc1732304b
paanini-kartik <paanini.kartik@gmail.com>
2026-03-21 12:35:10 -0400

    Creating api routes and making first draft of user one

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-# from routes import users, appointments, clinics, benefits
+from routes import users, appointments, clinics, benefits
 
 app = FastAPI()
 
-# app.include_router(users.router, prefix="/api/users")
+app.include_router(users.router, prefix="/api/users")
 # app.include_router(appointments.router, prefix="/api/appointments")
 # app.include_router(clinics.router, prefix="/api/clinics")
 # app.include_router(benefits.router, prefix="/api/benefits")
---
+++ server/routes/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/{user_id}")
+def get_user(user_id: str):
+    return {
+        "id": user_id,
+        "name": "Alex",
+        "age": 34,
+        "occupation": "Software Developer",
+        "location": {
+            "lat": 43.6532,
+            "lng": -79.3832
+        },
+        "benefits": {
+            "dental": { "total": 1500, "used": 400 },
+            "vision": { "total": 600, "used": 0 },
+            "physio": { "total": 900, "used": 200 }
+        }
+    }
