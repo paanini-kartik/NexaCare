7d88d7077b0ca3ff89d063636b72c607211a4e18
paanini-kartik <paanini.kartik@gmail.com>
2026-03-21 15:01:05 -0400

    Fixing main.py

--- server/main.py
+++ server/main.py
@@ -1,6 +1,12 @@
+import firebase
+
 from fastapi import FastAPI
-from routes import clinics
+from routes import users, appointments, clinics, benefits, auth
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix="/api/auth")
+app.include_router(users.router, prefix="/api/users")
+app.include_router(appointments.router, prefix="/api/appointments")
 app.include_router(clinics.router, prefix="/api/clinics")
+app.include_router(benefits.router, prefix="/api/benefits")
