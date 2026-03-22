f38368ecb5210ca9dba98700b005cd92b2d7a534
Kolby33 <kolbykatz@gmail.com>
2026-03-21 21:19:12 -0400

    Initialize Firebase SDK and environment variables

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import ScrollToTop from "./components/ScrollToTop";
 import { AuthProvider } from "./contexts/AuthContext";
+import "./lib/firebase";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
